e5d10546c7d98e81ad1c1c313c688648da49a5d1
Rishu Raj <sharmarishu695@gmail.com>
2026-01-25 13:49:02 +0530

    improved sollution for findMissingNumber

--- arrays/findMissingNumber/findMissingNumber.go
+++ arrays/findMissingNumber/findMissingNumber.go
@@ -1,41 +1,12 @@
 package findmissingnumber
 
 func missingNumber(nums []int) int {
-	quickSort(nums, 0, len(nums)-1)
+	numsLength := len(nums)
+	result := numsLength * (numsLength + 1) / 2
 
-	for i := 0; i < len(nums); i++ {
-		if (nums[i] != i) {
-			return i
-		}
-	}
-	return len(nums)
-}
-
-func quickSort(nums []int, low, high int) {
-	if (low < high) {
-		pivot := partition(nums, low, high)
-		quickSort(nums, low, pivot - 1)
-		quickSort(nums, pivot + 1, high)
-	}
-}
-
-func partition(nums []int, low, high int) int {
-	pivot := nums[high]
-	i := low - 1
-
-	for j := low; j < high; j++ {
-		if (nums[j] <= pivot) {
-			i++;
-			temp := nums[j]
-			nums[j] = nums[i]
-			nums[i] = temp
-		}
+	for _, value := range nums {
+		result -= value
 	}
 
-	i++
-
-	nums[high] = nums[i]
-	nums[i] = pivot
-
-	return i
+	return result
 }
