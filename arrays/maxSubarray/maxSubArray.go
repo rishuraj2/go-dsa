1c09a0efc8c7ac6ed71626bf1171ede9a26f3f19
Rishu Raj <sharmarishu695@gmail.com>
2026-03-10 00:07:04 +0530

    added solution for binary search

---
+++ arrays/maxSubarray/maxSubArray.go
@@ -0,0 +1,5 @@
+package maxsubarray
+
+func maxSubArray(nums []int) int {
+	 
+}
---
+++ binarySearch/binarySearch.go
@@ -0,0 +1,20 @@
+package binarysearch
+
+func search(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low < high {
+		mid := low + (high - low)/2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if target < nums[mid] {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
