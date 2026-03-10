e1e93703cd770629400b471b829965be2a4d09ce
Rishu Raj <sharmarishu695@gmail.com>
2026-03-11 01:37:56 +0530

    added solution for findMinInRotatedArray

---
+++ binarySearch/findMinInRotatedArray.go
@@ -0,0 +1,31 @@
+package binarysearch
+
+func findMin(nums []int) int {
+	low := 0
+	high := len(nums) - 1
+	minVal := nums[0]
+
+	for low <= high {
+		if nums[low] <= nums[high] {
+			return nums[low]
+		}
+
+		mid := low + (high - low)/2
+		minVal = min(minVal, nums[mid])
+		if nums[low] <= nums[mid] {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return minVal
+}
+
+func min(a, b int) int {
+	if a >= b {
+		return b
+	} else {
+		return a
+	}
+}
